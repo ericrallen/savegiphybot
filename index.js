cc2de649e87b14d7d34e3f21672f8f214f054feb
Eric Allen <decoy@internetalche.me>
2016-04-08 13:21:27 -0400

    added #savegiphy!

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var Botkit = require('botkit');
+
+var token = process.env.SLACK_TOKEN;
+
 var controller = Botkit.slackbot();
+
 var bot = controller.spawn({
-  token: my_slack_bot_token
-})
+  token: token
+});
+
 bot.startRTM(function(err,bot,payload) {
   if (err) {
     throw new Error('Could not connect to Slack');
   }
+});
+
+controller.hears(["/gif","*"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+  // do something to respond to message
+  // all of the fields available in a normal Slack message object are available
+  // https://api.slack.com/events/message
+  bot.reply(message,'#savegiphy!');
 });
